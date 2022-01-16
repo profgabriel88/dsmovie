73355690f60224f238d261c364375adf30f65ced
profgabriel88 <gabrieldai88@gmail.com>
2022-01-15 20:09:14 -0400

    useState useEffect

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,21 +1,30 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    // teste de requisição - inadequado
+    const [pageNumber, setPageNumber] = useState(0);
+
     let url = BASE_URL + "/movies?size=12&page=0";
-    axios.get(url).then(
-        response => {
-            console.log(response.data);
-        }
-    );
-    
+
+    useEffect(() => {
+        axios.get(url).then(
+            response => {
+                console.log(response.data);
+                const data = response.data as MoviePage;
+                setPageNumber(data.number);
+            }
+        );
+    }, [])  
 
     return (
         <>
+            <p>{pageNumber}</p>
+        
             <Pagination />
             <div className="container">
                 <div className="row">
